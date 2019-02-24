6c4bce94dccee263ca9ed27342cbddb87e7caf31
kaibush <kaibush@163.com>
2019-02-24 15:34:29 +0800

    easy125-191

---
+++ Easy125-191.py
@@ -0,0 +1,565 @@
+125. 验证回文串
+给定一个字符串，验证它是否是回文串，只考虑字母和数字字符，可以忽略字母的大小写。
+
+说明：本题中，我们将空字符串定义为有效的回文串。
+
+示例 1:
+
+输入: "A man, a plan, a canal: Panama"
+输出: true
+
+示例 2:
+
+输入: "race a car"
+输出: false
+
+
+class Solution(object):
+    def isPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        start, end = 0, len(s) - 1
+        while start < end:
+            if not s[start].isalnum():
+                start += 1
+                continue
+            if not s[end].isalnum():
+                end -= 1
+                continue
+            if s[start].lower() !=  s[end].lower():
+                return False
+            start += 1
+            end -= 1
+        return True
+        
+136. 只出现一次的数字
+给定一个非空整数数组，除了某个元素只出现一次以外，其余每个元素均出现两次。找出那个只出现了一次的元素。
+
+说明：
+
+你的算法应该具有线性时间复杂度。 你可以不使用额外空间来实现吗？
+
+示例 1:
+
+输入: [2,2,1]
+输出: 1
+
+示例 2:
+
+输入: [4,1,2,1,2]
+输出: 4
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        d = {}
+        for i in nums:
+            if i not in d:
+                d[i] = 0
+            else:
+                d.pop(i)
+        return d.keys()[0]
+
+异或：相同为0，不同为1. 异或同一个数两次，原数不变。
+class Solution(object):
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        for i in xrange(1, len(nums)):
+            nums[0] ^= nums[i]
+        return nums[0]
+
+141. 环形链表
+给定一个链表，判断链表中是否有环。
+
+为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。
+
+ 
+
+示例 1：
+
+输入：head = [3,2,0,-4], pos = 1
+输出：true
+解释：链表中有一个环，其尾部连接到第二个节点。
+
+示例 2：
+
+输入：head = [1,2], pos = 0
+输出：true
+解释：链表中有一个环，其尾部连接到第一个节点。
+
+示例 3：
+
+输入：head = [1], pos = -1
+输出：false
+解释：链表中没有环。
+
+ 
+
+进阶：
+
+你能用 O(1)（即，常量）内存解决此问题吗？
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def hasCycle(self, head):
+        """
+        :type head: ListNode
+        :rtype: bool
+        """
+        faster = slower = head
+        while faster and faster.next:
+            faster = faster.next.next
+            slower = slower.next
+            if slower == faster:
+                return True
+        return False
+
+155. 最小栈
+设计一个支持 push，pop，top 操作，并能在常数时间内检索到最小元素的栈。
+
+    push(x) -- 将元素 x 推入栈中。
+    pop() -- 删除栈顶的元素。
+    top() -- 获取栈顶元素。
+    getMin() -- 检索栈中的最小元素。
+
+示例:
+
+MinStack minStack = new MinStack();
+minStack.push(-2);
+minStack.push(0);
+minStack.push(-3);
+minStack.getMin();   --> 返回 -3.
+minStack.pop();
+minStack.top();      --> 返回 0.
+minStack.getMin();   --> 返回 -2.
+
+
+class MinStack(object):
+
+    def __init__(self):
+        """
+        initialize your data structure here.
+        """
+        self.stack = []
+
+    def push(self, x):
+        """
+        :type x: int
+        :rtype: void
+        """
+        if not self.stack:
+            self.stack.append((x, x))
+        else:
+            self.stack.append((x, min(x, self.stack[-1][-1])))
+        
+
+    def pop(self):
+        """
+        :rtype: void
+        """
+        self.stack.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1][0]
+        
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1][1]
+
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(x)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+
+
+        
+160. 相交链表
+编写一个程序，找到两个单链表相交的起始节点。
+
+如下面的两个链表：
+
+在节点 c1 开始相交。
+
+ 
+
+示例 1：
+
+输入：intersectVal = 8, listA = [4,1,8,4,5], listB = [5,0,1,8,4,5], skipA = 2, skipB = 3
+输出：Reference of the node with value = 8
+输入解释：相交节点的值为 8 （注意，如果两个列表相交则不能为 0）。从各自的表头开始算起，链表 A 为 [4,1,8,4,5]，链表 B 为 [5,0,1,8,4,5]。在 A 中，相交节点前有 2 个节点；在 B 中，相交节点前有 3 个节点。
+
+ 
+
+示例 2：
+
+输入：intersectVal = 2, listA = [0,9,1,2,4], listB = [3,2,4], skipA = 3, skipB = 1
+输出：Reference of the node with value = 2
+输入解释：相交节点的值为 2 （注意，如果两个列表相交则不能为 0）。从各自的表头开始算起，链表 A 为 [0,9,1,2,4]，链表 B 为 [3,2,4]。在 A 中，相交节点前有 3 个节点；在 B 中，相交节点前有 1 个节点。
+
+ 
+
+示例 3：
+
+输入：intersectVal = 0, listA = [2,6,4], listB = [1,5], skipA = 3, skipB = 2
+输出：null
+输入解释：从各自的表头开始算起，链表 A 为 [2,6,4]，链表 B 为 [1,5]。由于这两个链表不相交，所以 intersectVal 必须为 0，而 skipA 和 skipB 可以是任意值。
+解释：这两个链表不相交，因此返回 null。
+
+ 
+
+注意：
+
+    如果两个链表没有交点，返回 null.
+    在返回结果后，两个链表仍须保持原有的结构。
+    可假定整个链表结构中没有循环。
+    程序尽量满足 O(n) 时间复杂度，且仅用 O(1) 内存。
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution(object):
+    def getIntersectionNode(self, headA, headB):
+        """
+        :type head1, head1: ListNode
+        :rtype: ListNode
+        定义两个指针, 第一轮让两个到达末尾的节点指向另一个链表的头部, 
+        最后如果相遇则为交点(在第一轮移动中恰好抹除了长度差)
+        两个指针等于移动了相同的距离, 有交点就返回, 无交点就是各走了两条指针的长度
+        """
+        if headA is None and headB is None:
+            return None
+        pa = headA
+        pb = headB 
+        while pa is not pb:
+            pa = headB if pa is None else pa.next
+            pb = headA if pb is None else pb.next
+        return pa
+        
+167. 两数之和 II - 输入有序数组
+给定一个已按照升序排列 的有序数组，找到两个数使得它们相加之和等于目标数。
+
+函数应该返回这两个下标值 index1 和 index2，其中 index1 必须小于 index2。
+
+说明:
+
+    返回的下标值（index1 和 index2）不是从零开始的。
+    你可以假设每个输入只对应唯一的答案，而且你不可以重复使用相同的元素。
+
+示例:
+
+输入: numbers = [2, 7, 11, 15], target = 9
+输出: [1,2]
+解释: 2 与 7 之和等于目标数 9 。因此 index1 = 1, index2 = 2 。
+
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        d = {}
+        for id, num in enumerate(numbers):
+            if target - num not in d:
+                d[num] = id
+            else:
+                return [d[target-num]+1, id+1]
+        return None
+        
+class Solution(object):
+    def twoSum(self, numbers, target):
+        """
+        :type numbers: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        start, end = 0, len(numbers) - 1
+        while start < end:
+            s = numbers[start] + numbers[end]
+            if target > s:
+                start += 1
+            elif target < s:
+                end -= 1
+            else:
+                return start + 1, end + 1
+
+168. Excel表列名称
+给定一个正整数，返回它在 Excel 表中相对应的列名称。
+
+例如，
+
+    1 -> A
+    2 -> B
+    3 -> C
+    ...
+    26 -> Z
+    27 -> AA
+    28 -> AB 
+    ...
+
+示例 1:
+
+输入: 1
+输出: "A"
+
+示例 2:
+
+输入: 28
+输出: "AB"
+
+示例 3:
+
+输入: 701
+输出: "ZY"
+
+class Solution(object):
+    def convertToTitle(self, n):
+        """
+        :type n: int
+        :rtype: str
+        """
+        init = ord('A') - 1
+        d = {}
+        for i in range(1, 27):
+            d[i] = chr(init + i)
+        s = ''
+        ans = ''
+        while n:
+            m = n % 26
+            if m == 0:
+                ans = 'Z' + ans
+                n -= 26
+            else:
+                ans = d[m] + ans
+            n = n / 26
+        return ans
+        
+        
+169. 求众数
+给定一个大小为 n 的数组，找到其中的众数。众数是指在数组中出现次数大于 ⌊ n/2 ⌋ 的元素。
+
+你可以假设数组是非空的，并且给定的数组总是存在众数。
+
+示例 1:
+
+输入: [3,2,3]
+输出: 3
+
+示例 2:
+
+输入: [2,2,1,1,1,2,2]
+输出: 2
+
+class Solution(object):
+    def majorityElement(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        摩尔投票算法
+        """
+        cnt, res = 0, 0
+        for num in nums:
+            print cnt,':', num, res
+            if cnt == 0:
+                res = num
+            if num != res:
+                cnt -= 1
+            else:
+                cnt += 1
+        return res
+
+171. Excel表列序号
+给定一个Excel表格中的列名称，返回其相应的列序号。
+
+例如，
+
+    A -> 1
+    B -> 2
+    C -> 3
+    ...
+    Z -> 26
+    AA -> 27
+    AB -> 28 
+    ...
+
+示例 1:
+
+输入: "A"
+输出: 1
+
+示例 2:
+
+输入: "AB"
+输出: 28
+
+示例 3:
+
+输入: "ZY"
+输出: 701
+
+class Solution(object):
+    def titleToNumber(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        ans = 0
+        m = 0
+        for n in reversed(s):
+            ans += (ord(n)-64) * 26 ** m
+            m += 1
+        return ans
+
+172. 阶乘后的零
+给定一个整数 n，返回 n! 结果尾数中零的数量。
+
+示例 1:
+
+输入: 3
+输出: 0
+解释: 3! = 6, 尾数中没有零。
+
+示例 2:
+
+输入: 5
+输出: 1
+解释: 5! = 120, 尾数中有 1 个零.
+
+class Solution(object):
+    def trailingZeroes(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        a = 0
+        while n:
+            n /= 5
+            a += n
+        return a
+
+        
+189. 旋转数组
+给定一个数组，将数组中的元素向右移动 k 个位置，其中 k 是非负数。
+
+示例 1:
+
+输入: [1,2,3,4,5,6,7] 和 k = 3
+输出: [5,6,7,1,2,3,4]
+解释:
+向右旋转 1 步: [7,1,2,3,4,5,6]
+向右旋转 2 步: [6,7,1,2,3,4,5]
+向右旋转 3 步: [5,6,7,1,2,3,4]
+
+示例 2:
+
+输入: [-1,-100,3,99] 和 k = 2
+输出: [3,99,-1,-100]
+解释: 
+向右旋转 1 步: [99,-1,-100,3]
+向右旋转 2 步: [3,99,-1,-100]
+
+说明:
+
+    尽可能想出更多的解决方案，至少有三种不同的方法可以解决这个问题。
+    要求使用空间复杂度为 O(1) 的原地算法。
+    
+class Solution(object):
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        for i in range(k):
+            last = nums.pop()
+            nums.insert(0, last)
+
+    
+190. 颠倒二进制位
+颠倒给定的 32 位无符号整数的二进制位。
+
+ 
+
+示例 1：
+
+输入: 00000010100101000001111010011100
+输出: 00111001011110000010100101000000
+解释: 输入的二进制串 00000010100101000001111010011100 表示无符号整数 43261596，
+      因此返回 964176192，其二进制表示形式为 00111001011110000010100101000000。
+
+示例 2：
+
+输入：11111111111111111111111111111101
+输出：10111111111111111111111111111111
+解释：输入的二进制串 11111111111111111111111111111101 表示无符号整数 4294967293，
+      因此返回 3221225471 其二进制表示形式为 10101111110010110010011101101001。
+      
+class Solution:
+    # @param n, an integer
+    # @return an integer
+    def reverseBits(self, n):
+        ans = 0
+        for i in range(32):
+            pop = n & 1 # 求末尾的数字
+            ans = ans * 2 + pop
+            n = n >> 1
+        return ans
+
+191. 位1的个数
+编写一个函数，输入是一个无符号整数，返回其二进制表达式中数字位数为 ‘1’ 的个数（也被称为汉明重量）。
+
+ 
+
+示例 1：
+
+输入：00000000000000000000000000001011
+输出：3
+解释：输入的二进制串 00000000000000000000000000001011 中，共有三位为 '1'。
+
+示例 2：
+
+输入：00000000000000000000000010000000
+输出：1
+解释：输入的二进制串 00000000000000000000000010000000 中，共有一位为 '1'。
+
+示例 3：
+
+输入：11111111111111111111111111111101
+输出：31
+解释：输入的二进制串 11111111111111111111111111111101 中，共有 31 位为 '1'。
+
+class Solution(object):
+    def hammingWeight(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        cnt = 0
+        for i in range(32):
+            pop = n & 1
+            if pop:
+                cnt += 1
+            n = n >> 1
+        return cnt
